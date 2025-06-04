2b81de357156fe98bba757ac82315a2b02725125
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-06-04 12:34:02 +0200

    milestone 1

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+// 📌 Milestone 1
+// Crea un type alias Person per rappresentare una persona generica.
+
+// Il tipo deve includere le seguenti proprietà:
+
+// id: numero identificativo, non modificabile
+// name: nome completo, stringa non modificabile
+// birth_year: anno di nascita, numero
+// death_year: anno di morte, numero opzionale
+// biography: breve biografia, stringa
+// image: URL dell'immagine, stringa
+
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
